a298e7a2a2906b871d1520d97836b9e1e83f89d1
Sourav Ghosh <susmitakatwa92@gmail.com>
2025-07-21 23:07:24 +0530

    updated app.py file

--- app.py
+++ app.py
@@ -1,44 +1,39 @@
 import pickle
 import streamlit as st
-import requests
+import gdown
 import os
+import requests
 
-# Function to download files from Google Drive or other direct link
-def download_file(url, destination):
+# Function to download files from Google Drive using gdown
+def download_file_from_drive(file_id, destination):
     if not os.path.exists(destination):
         st.write(f"Downloading {destination}...")
-        response = requests.get(url)
-        with open(destination, 'wb') as f:
-            f.write(response.content)
-        st.write(f"{destination} downloaded successfully.")
+        url = f'https://drive.google.com/uc?id={file_id}'
+        gdown.download(url, destination, quiet=False)
+        st.write(f"{destination} downloaded successfully. Size: {os.path.getsize(destination)} bytes.")
     else:
         st.write(f"{destination} already exists. Skipping download.")
 
 # Download the .pkl files if not present
-movies_url = 'https://drive.google.com/uc?export=download&id=1Cdi5TbnaPbp5lDjKiZ8KWkAO8P0YJ6fY'
+movies_file_id = '1Cdi5TbnaPbp5lDjKiZ8KWkAO8P0YJ6fY'
+similarity_file_id = '1eVHczxlSOz-aKZqW_1A2KhE61SmNNaxX'
 
-
-similarity_url = 'https://drive.google.com/uc?export=download&id=1eVHczxlSOz-aKZqW_1A2KhE61SmNNaxX' 
-
-
-download_file(movies_url, 'new_movies.pkl')
-download_file(similarity_url, 'new_similarity.pkl')
-
+download_file_from_drive(movies_file_id, 'new_movies.pkl')
+download_file_from_drive(similarity_file_id, 'new_similarity.pkl')
 
 # Load the downloaded pickle files
-movies = pickle.load(open('new_movies.pkl','rb'))
-similarity = pickle.load(open('new_similarity.pkl','rb'))
+movies = pickle.load(open('new_movies.pkl', 'rb'))
+similarity = pickle.load(open('new_similarity.pkl', 'rb'))
 
 # Fetch poster from TMDB
 def fetch_poster(id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=55a1eb05ce3d68972a463b781ef08a30&language=en-US".format(id)
+    url = f"https://api.themoviedb.org/3/movie/{id}?api_key=55a1eb05ce3d68972a463b781ef08a30&language=en-US"
     try:
         data = requests.get(url, timeout=10)
         data = data.json()
         poster_path = data.get('poster_path')
         if poster_path:
-            full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-            return full_path
+            return "https://image.tmdb.org/t/p/w500/" + poster_path
         else:
             return "https://via.placeholder.com/500x750?text=No+Image"
     except Exception as e:
@@ -52,11 +47,10 @@ def recommend(movie):
     recommended_movie_names = []
     recommended_movie_posters = []
     for i in distances[1:6]:
-        id = movies.iloc[i[0]].id
-        recommended_movie_posters.append(fetch_poster(id))
+        movie_id = movies.iloc[i[0]].id
+        recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
     return recommended_movie_names, recommended_movie_posters
-
 
 # Streamlit UI
 st.header('🎬 Movie Recommender System')
